d5b07fbeecb0a1621f7fe0a9c02d89a817018b75
Landen <landenheine17@gmail.com>
2026-01-15 16:20:10 -0700

    Updated Gradle and added simple logic to TeleOp Main
    Servo's Launch using left bumper and the flywheel is controlled using the right bumper, the drivetrain is controlled directly using the stucks, currently it is swapped and needs righted

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotHardware.java
@@ -1,14 +1,25 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class RobotHardware {
     DcMotor leftMotor, rightMotor;
+    DcMotorEx flywheelMotor;
+    Servo leftServo, rightServo;
 
     public void init(HardwareMap HwMap) {
-
+        // MOTORS
         leftMotor = HwMap.get(DcMotor.class, "leftMotor");
         rightMotor = HwMap.get(DcMotor.class, "rightMotor");
+        flywheelMotor = HwMap.get(DcMotorEx.class, "flywheelMotor");
+
+        // SERVOS
+        leftServo = HwMap.get(Servo.class, "leftServo");
+        rightServo = HwMap.get(Servo.class, "rightServo");
+
+
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMain.java
@@ -15,10 +15,31 @@ public class TeleOpMain extends OpMode {
     public void init() {
         rob.init(hardwareMap);
     }
+
     // This runs in a loop after the user presses play
     @Override
     public void loop() {
+        // Controls drivetrain
         rob.leftMotor.setPower(gamepad1.left_stick_y);
         rob.rightMotor.setPower(gamepad1.right_stick_y);
+
+        // Controls flywheels
+        if (gamepad1.right_bumper) {
+            rob.flywheelMotor.setPower(1);
+        } else {
+            rob.flywheelMotor.setPower(0);
+        }
+
+        // Controls Servos
+        if (gamepad1.left_bumper) {
+            rob.leftServo.setPosition(1);
+            rob.rightServo.setPosition(0);
+        } else {
+            rob.leftServo.setPosition(0);
+            rob.rightServo.setPosition(1);
+        }
+        telemetry.addData("Flywheel ticks/sec", rob.flywheelMotor.getVelocity());
+        telemetry.addData("Flywheel power", rob.flywheelMotor.getPower());
+        telemetry.update();
     }
 }
